30a7544a4d296c617446b2b5a838652cc27ab00f
akkowal2 <akkowal2@illinois.edu>
2015-02-18 16:20:12 -0600

    fixed a bug with test_bitmessage not adding the module to the system path.  Also testing to see if travis can successfully install and run bitmessage

--- unittests/test_bitmessage.py
+++ unittests/test_bitmessage.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import os
 
 class TestBitmessage(unittest.TestCase):
     def setUp(self):
@@ -13,6 +14,8 @@ class TestBitmessage(unittest.TestCase):
 
 if __name__ == '__main__':
     sys.path.append("../bitmessage/")
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    sys.path.insert(1, path)
     from bitmessage.bitmessage import Bitmessage
 
     suite = unittest.TestLoader().loadTestsFromTestCase(TestBitmessage)
