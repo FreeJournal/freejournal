2721a91374172d860df3b611662d27ebc9a98b2e
Philip Daian <phil@linux.com>
2015-02-23 12:43:01 -0600

    Add project configuration file with sample

---
+++ config.py
@@ -0,0 +1,19 @@
+''' URL to use for the SQLAlchemy engine.
+    By default, the Postgres database is used and required.
+    Feel free to change this to any of the commented, alternate
+    URL's to change database management systems.
+''' 
+DB_URL = 'postgres://postgres:password@localhost/freejournal'
+
+# DB_URL = 'mysql://scott:tiger@localhost/foo'
+# DB_URL = 'sqlite:///foo.db'
+
+''' Caching strategy.
+    'all': attempt to mirror all documents within diskspace constraints
+    ('blacklist', [address array]): don't mirror collections on blacklist array
+    ('whitelist', [address array]): don't mirror collections on blacklist array'''
+MIRROR_STRATEGY = 'all'
+# MIRROR_STRATEGY = ('blacklist', ['BM-1EXAMPLE'])
+
+''' Max disk space to use.  Allowable units are G and M.'''
+MAX_DISKSPACE = '5G'
