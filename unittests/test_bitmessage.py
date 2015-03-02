61a34c33adc9bfe19f351c7b9fa8cd3141e74ea7
akkowal2 <akkowal2@illinois.edu>
2015-03-02 13:39:07 -0600

    attempting to have test_bitmessage successfully pass with travis

--- unittests/test_bitmessage.py
+++ unittests/test_bitmessage.py
@@ -1,3 +1,4 @@
+from global_imports import Bitmessage
 import unittest
 import sys
 import os
@@ -78,10 +79,5 @@ class TestBitmessage(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    sys.path.append("../bitmessage/")
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    sys.path.insert(1, path)
-    from bitmessage.bitmessage import Bitmessage
-
     suite = unittest.TestLoader().loadTestsFromTestCase(TestBitmessage)
     unittest.TextTestRunner(verbosity=2).run(suite)
