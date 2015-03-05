dc5780f9446ac55aa434ff67747353eea05f4cb8
dzebro2 <dzebro2@illinois.edu>
2015-03-04 19:03:18 -0600

    trying to fix imports v3

--- imports.py
+++ imports.py
@@ -4,15 +4,18 @@ import sys
 from config import *
 from cache.cache import *
 
+sys.path.append(os.path.expanduser('~/PyBitmessage/src'))
 
-path = os.path.abspath(os.path.join(os.path.dirname('~/PyBitmessage/src/')))
-sys.path.insert(0, path)
-import addresses
 
 Bitmessage = getattr(__import__('bitmessage.bitmessage', fromlist=['']), 'Bitmessage')
 Collection = getattr(__import__('models.collection', fromlist=['']), 'Collection')
-FJMessage = getattr(__import__('backend.datastructures.fj_message', fromlist=['']), 'FJMessage')
 Document = getattr(__import__('models.document', fromlist=['']), 'Document')
 Keyword = getattr(__import__('models.keyword', fromlist=['']), 'Keyword')
-CollectionHandler = getattr(__import__('backend.collection_handler', fromlist=['']), 'CollectionHandler')
+try:
+	import addresses
+	FJMessage = getattr(__import__('backend.datastructures.fj_message', fromlist=['']), 'FJMessage')
+	CollectionHandler = getattr(__import__('backend.collection_handler', fromlist=['']), 'CollectionHandler')
+except (ImportError):
+	pass
+
 
