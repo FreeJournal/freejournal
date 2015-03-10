4b2ff5299657e84b4584cd4977657191bf0c4966
Walker Henderson <wjahenderson@gmail.com>
2015-03-10 15:25:17 -0500

    Fixed merge issue

--- run_server.py
+++ run_server.py
@@ -4,5 +4,5 @@ import config
 celery = celery  # Give `celery` variable to pass to celery
 
 if __name__ == '__main__':
-    app.debug = config.WEB_DEBUG
+    app.debug = config.WEBAPP_DEBUG
     app.run()
