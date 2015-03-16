e0d2df7970acf9ba4abeccacbea91a27429cc7e6
dzebro2 <dzebro2@illinois.edu>
2015-03-16 01:16:03 -0500

    forgot a file

---
+++ models/json_schemas.py
@@ -0,0 +1,123 @@
+coll_schema = {
+        "type": "object",
+
+        "properties": {
+            "type_id": {
+                "type": "integer"
+            },
+            "title": {
+                "type": "string"
+            },
+            "description": {
+                "type": "string"
+            },
+            "merkle": {
+                "type": "string"
+            },
+            "address": {
+                "type": "string"
+            },
+            "version": {
+                "type": "integer"
+            },
+            "btc": {
+                "type": "string"
+            },
+            "keywords": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "id": {
+                            "type": "integer"
+                        },
+                        "name": {
+                            "type": "string"
+                        }
+                    },
+                    "required": ["id", "name"]
+                }
+            },
+            "documents": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "address": {
+                            "type": "string"
+                        },
+                        "hash": {
+                            "type": "string"
+                        },
+                        "description": {
+                            "type": "string"
+                        },
+                        "title": {
+                            "type": "string"
+                        },
+                        "filename": {
+                            "type": "string"
+                        },
+                        "accesses": {
+                            "type": "integer"
+                        }
+                    },
+                    "required": ["address", "hash", "title", "description", "filename", "accesses"]
+
+                }
+            },
+            "latest_broadcast_date": {
+                "type": "string"
+            },
+            "creation_date": {
+                "type": "string"
+            },
+            "oldest_date": {
+                "type": "string"
+            },
+            "latest_btc_tx": {
+                "type": "string"
+            },
+            "oldest_btc_tx": {
+                "type": "string"
+            },
+            "accesses": {
+                "type": "integer"
+            },
+            "votes": {
+                "type": "integer"
+            },
+            "votes_last_checked": {
+                "type": "string"
+            }
+        },
+        "required": ["type_id", "title", "description", "documents", "keywords", "version", "merkle", "address", "btc",
+                     "creation_date", "oldest_date", "latest_btc_tx", "oldest_btc_tx",
+                     "latest_broadcast_date", "accesses", "votes", "votes_last_checked"]
+
+}
+
+fj_schema = {
+    "type": "object",
+    "properties": {
+        "protocol": {
+            "type": "string"
+        },
+        "type_id": {
+            "type": "integer"
+        },
+        "original_sender": {
+            "type": "string"
+        },
+        "signature": {
+            "type": "string"
+        },
+        "time_created": {
+            "type": "string"
+        },
+        "pubkey": {
+            "type": "string"
+        },
+    },
+    "required": ["protocol", "type_id", "original_sender", "signature", "time_created", "pubkey"]
+}
