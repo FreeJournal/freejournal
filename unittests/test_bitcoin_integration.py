76b6558cc0b7cfe13ca47530ee6c83adf667b192
esco <bmdahmen@gmail.com>
2015-04-14 12:26:19 -0500

    bitcoin integration fixed

--- unittests/test_bitcoin_integration.py
+++ unittests/test_bitcoin_integration.py
@@ -56,26 +56,6 @@ class TestBitcoinIntegration(unittest.TestCase):
     #     self.assertEqual(self.collection.oldest_btc_tx, "2015-03-01 06:42:08;2be396b3c08c71c04e750142eda9216f52cdd4277345a239ec736fe2540f5e53")
     #     self.assertEqual(self.collection.latest_btc_tx, "2015-03-16 02:19:19;c6788150e4a409c653f7244da3ebfef3fa7cfb5faf87d3e31fd997d42bdfde7b")
 
-    def test_update_collection(self):
-        self.collection.update_timestamp()
-        self.assertEqual(self.collection.latest_btc_tx, "2015-03-16 02:19:19;c6788150e4a409c653f7244da3ebfef3fa7cfb5faf87d3e31fd997d42bdfde7b")
-        self.assertEqual(self.collection.oldest_btc_tx, "2015-03-16 02:19:19;c6788150e4a409c653f7244da3ebfef3fa7cfb5faf87d3e31fd997d42bdfde7b")
-
-    def test_update_collection_older(self):
-        self.collection.update_timestamp()
-        self.collection.merkle =  "783701f7599830824fa73488f80eb79894f6f14203264b6a3ac3f0a14012c25f"
-        self.collection.update_timestamp()
-        self.assertEqual(self.collection.oldest_btc_tx, "2015-03-01 06:42:08;2be396b3c08c71c04e750142eda9216f52cdd4277345a239ec736fe2540f5e53")
-        self.assertEqual(self.collection.latest_btc_tx, "2015-03-16 02:19:19;c6788150e4a409c653f7244da3ebfef3fa7cfb5faf87d3e31fd997d42bdfde7b")
-
-    def test_update_collection_newer(self):
-        self.collection.merkle =  "783701f7599830824fa73488f80eb79894f6f14203264b6a3ac3f0a14012c25f"
-        self.collection.update_timestamp()
-        self.collection.merkle =  "f61504ae086c136511e7982c5d638a155c8a88832cd35c35c32f7de23ca5ce54"
-        self.collection.update_timestamp()
-        self.assertEqual(self.collection.oldest_btc_tx, "2015-03-01 06:42:08;2be396b3c08c71c04e750142eda9216f52cdd4277345a239ec736fe2540f5e53")
-        self.assertEqual(self.collection.latest_btc_tx, "2015-03-16 02:19:19;c6788150e4a409c653f7244da3ebfef3fa7cfb5faf87d3e31fd997d42bdfde7b")
-
 suite = unittest.TestLoader().loadTestsFromTestCase(TestBitcoinIntegration)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
