a74d4e49a020294645be27340d0ad67523c3f875
akkowal2 <akkowal2@illinois.edu>
2015-04-23 11:29:42 -0500

    changed the freejournal main channel

--- config.py
+++ config.py
@@ -24,9 +24,10 @@ RUN_PYBITMESSAGE_LINUX = "python ~/PyBitmessage/src/bitmessagemain.py"
 BITMESSAGE_SERVER = "http://username:password@0.0.0.0:8442/"
 
 ''' Don't change this unless the main channel address is changed in the future '''
-MAIN_CHANNEL_ADDRESS = "BM-2cWvQ4HqcxLhgKjc5zkuwsbqf69mryY5mr"
-privsigningkey = "5JscnTKKubbtQepFUzFjKm9SjBMQ7PZKgd8EnauXZGwLjPVF2zs"
-privencryptionkey = "5K9KRL9KffdbWcBqzPeXv5Rfo2YRJFD3SASJmXJsZkEphy4Rh6q"
+
+MAIN_CHANNEL_ADDRESS = "BM-2cVqkJCqk9RTapvmVwEd2mrKUnzLoAcHLx"
+privsigningkey = "5J15mbNV5dWEvLcdVf2wua2Xn3Hn73qnNuacVcYt3zo8CKeSwCE"
+privencryptionkey = "5JBZS85dLU8QTK5gPxH3YGnEgiGyaB8RjQ9J6yEuTsAtSTrbZYR"
 
 ''' Directory to store documents that are downloaded
 (Make sure to keep the last '/' at the end of the path)'''
